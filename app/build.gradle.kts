290e6cf0172165b0cfdb1a6c923fff8864da22af
YanaKudrinskaya <yanakudrinskaya86@gmail.com>
2025-10-08 17:01:29 +0300

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,34 +42,50 @@ android {
 }
 
 dependencies {
+    // Room
     implementation(libs.android.room)
     implementation(libs.android.room.ktx)
     kapt(libs.android.room.compiler)
 
+    // AndroidX
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.viewpager2)
     implementation(libs.androidx.fragment)
-    implementation(libs.io)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.constraintlayout)
 
+    // Material
+    implementation(libs.material)
+
+    // Image Loading
+    implementation(libs.glide)
+
+    // Koin dependency injection
+    implementation(libs.koin)
+
+    // Тестирование
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Firebase
     implementation(libs.firebase.bom)
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth.ktx)
 
-    implementation(libs.glide)
+    // GSON
     implementation(libs.gson)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.annotation)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    // Google Auth
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
 }
